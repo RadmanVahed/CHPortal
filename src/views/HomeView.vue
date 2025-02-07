4ed8af7441846f2f93453e47a7f893b4532764c9
RadmanVahed <radmanvahed@gmail.com>
2025-02-07 22:16:15 +0330

    Initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const items = ref([
+  {
+    label: 'Icons',
+    icon: 'i-lucide-smile',
+    content: 'You have nothing to do, @nuxt/icon will handle it automatically.'
+  },
+  {
+    label: 'Colors',
+    icon: 'i-lucide-swatch-book',
+    content: 'Choose a primary and a neutral color from your Tailwind CSS theme.'
+  },
+  {
+    label: 'Components',
+    icon: 'i-lucide-box',
+    content: 'You can customize components by using the `class` / `ui` props or in your app.config.ts.'
+  }
+])
+</script>
+
+<template>
+  
+  <UButton size="md"  to="/login">ثبت نام</UButton>
+</template>
