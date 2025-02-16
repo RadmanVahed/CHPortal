7250e6def9bc3aae2c22a05fc9c233d36156b32d
RadmanVahed <radmanvahed@gmail.com>
2025-02-16 11:26:39 +0330

    Updates

--- assets/AdminView-C_1wIBFd.js
+++
@@ -1,1 +0,0 @@
-import{_ as s}from"./Separator.vue_vue_type_script_setup_true_lang-BHicULUh.js";import{d as t,c as n,b as o,a as r,F as c,o as l}from"./index-DYkGDyiV.js";const f=t({__name:"AdminView",setup(m){return(_,e)=>{const a=s;return l(),n(c,null,[o(a,{class:"my-8",label:"داشبورد مدیریت"}),e[0]||(e[0]=r("div",{class:"flex justify-center"},null,-1))],64)}}});export{f as default};
